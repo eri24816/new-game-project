00b4fd793ca54090159fdc16a34712fdb88a1bda
eri24816 <a931eric@gmail.com>
2024-09-14 14:57:48 +0800

    first commit

---
+++ test.gd
@@ -0,0 +1,12 @@
+extends MeshInstance3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	# move left by 1 unit per second
+	translate(Vector3(-1, 0, 0) * delta)
